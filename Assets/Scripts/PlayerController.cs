e4e686371662befcb8fe0cb8d7d72f5c97ba4af8
aianman4823 <akitoaianman1@gmail.com>
2023-02-12 19:22:10 +0900

    OnCollisionEnterの実装

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float jampPower = 5.0f;
+    int jumpCount = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +16,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("space") == true)
+        if (Input.GetKey("right") == true)
         {
-            this.gameObject.GetComponent<BoxCollider>().isTrigger = true;
+            this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("left") == true)
+        {
+            this.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
         }
 
+        if (Input.GetKeyDown("space") == true && jumpCount < 2)
+        {
+            this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, jampPower, 0);
+            jumpCount++;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            jumpCount = 0;
+        }
     }
 }
