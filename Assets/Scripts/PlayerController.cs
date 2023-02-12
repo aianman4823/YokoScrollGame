df6c3ad879f5bbfee77afe626f68faf955168358
aianman4823 <akitoaianman1@gmail.com>
2023-02-12 18:46:15 +0900

    GetComponentの使い方

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sample : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("space") == true)
+        {
+            this.gameObject.GetComponent<BoxCollider>().isTrigger = true;
+        }
+
     }
 }
