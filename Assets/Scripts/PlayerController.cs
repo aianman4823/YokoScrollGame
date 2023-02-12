a482c527136a12104b9af3f73f9bd9b90ec43d39
aianman4823 <akitoaianman1@gmail.com>
2023-02-12 19:45:53 +0900

    音をつけた

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,14 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float jampPower = 5.0f;
     int jumpCount = 0;
+
+    public AudioClip pickUpSound;
+    public AudioClip jumpSound;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-
+        audioSource = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,6 +32,8 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown("space") == true && jumpCount < 2)
         {
             this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, jampPower, 0);
+            audioSource.clip = jumpSound;
+            audioSource.Play();
             jumpCount++;
         }
     }
@@ -37,6 +43,12 @@ public class PlayerController : MonoBehaviour
         if (col.gameObject.tag == "Ground")
         {
             jumpCount = 0;
+        }
+        if (col.gameObject.tag == "Coin")
+        {
+            audioSource.clip = pickUpSound;
+            audioSource.Play();
+            Destroy(col.gameObject);
         }
     }
 }
